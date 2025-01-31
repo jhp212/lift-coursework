0e36d696a5c309c686f3d7f850d78db9324425d4
jhp212 <jhp212@exeter.ac.uk>
2025-01-31 10:19:10 +0000

    added error handling for json file loading

--- sources/tests/loader.py
+++ sources/tests/loader.py
@@ -1,14 +1,40 @@
 import json
 
 def load_file(path:str):
+    # load json file as a python dictionary
     with open(path, "r") as file:
         data = json.load(file)
     
-    if data["floor_count"] < len(data["floor_requests"]):
-        raise ValueError("Too many floors given!")
+    #checking keys exists in the file
+    if "floor_count" not in data:
+        raise KeyError("floor_count does not exist!")
+    if "capacity" not in data:
+        raise KeyError("capacity does not exist!")
+    if "floor_requests" not in data:
+        raise KeyError("floor_requests does not exist!")
+    
+    #checking data types
+    if type(data["floor_count"]) != int:
+        raise TypeError("Floor count must be an integer!")
+    if type(data["capacity"]) != int:
+        raise TypeError("Capacity must be an integer!")
+    
+    #checking values are greater than 0
+    if data["floor_count"] <= 0:
+        raise ValueError("Floor count is <= 0")
+    if data["capacity"] <= 0:
+        raise ValueError("Capacity is <= 0")
+    
     
     passenger_list = []
     for floor in data["floor_requests"]:
+        try:
+            if int(floor) > data["floor_count"]:
+                raise ValueError(f"There is a passenger waiting on floor {floor} which is greater than the floor count!")
+            
+        except ValueError:
+            raise ValueError("Floor values must be integers inside strings! e.g '2'")
+        
         for end_floor in data["floor_requests"][floor]:
             passenger_list.append(Passenger(int(floor), end_floor, len(passenger_list)))
 
@@ -43,4 +69,4 @@ if __name__ == "__main__":
     print(capacity)
     for passenger in passenger_list:
         print((passenger.start_floor, passenger.end_floor))
-    
+    int("2.3")
