5cb5f633bf23a06f5e7e7670ca0ce2adba494daf
O-Robson <ojr209@exeter.ac.uk>
2025-01-31 09:50:01 +0000

    Moved passenger class

--- sources/tests/loader.py
+++ sources/tests/loader.py
@@ -2,3 +2,26 @@ import json
 
 def load_file(path:str):
     pass
+
+
+class Passenger:
+    def __init__(self, start_floor: int, end_floor: int, passenger_id: int):
+        self.start_floor, self.end_floor, self.start_time = start_floor, end_floor, 0
+        self.boarded, self.lift_id = False, None
+        self.passenger_id = passenger_id
+        self.direction = -1 if start_floor - end_floor > 0 else 1
+    
+    def get_on(self, lift_id: int):
+        self.boarded = True
+        self.lift_id = lift_id
+    
+    def get_off(self):
+        global lift_list # list[Lift]
+        if lift_list[self.lift_id].floor == self.end_floor:
+            self.boarded = False
+            self.lift_id = None
+            global iteration_count # int
+            self.end_time = iteration_count
+            return True
+        else:
+            return False
